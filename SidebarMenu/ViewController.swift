96af857feaef990745178bb7429e31543dea2c6b
Jorge Benavides <bogue89@gmail.com>
2016-05-16 18:09:45 -0500

    Initial Commit

---
+++ SidebarMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SidebarMenu
+//
+//  Created by Jorge Benavides on 5/16/16.
+//  Copyright © 2016 Jorge Benavides. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
