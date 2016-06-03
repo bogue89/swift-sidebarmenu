256b6d5f06d02319c725c79a4e4caf4b5e1cd836
Jorge Benavides <bogue89@gmail.com>
2016-06-02 20:21:50 -0500

    Table view and functionality for events on ProgramController. now working on the showed data

--- SidebarMenu/ViewController.swift
+++ SidebarMenu/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.layer.borderColor = UIColor.greenColor().CGColor
+        self.view.layer.borderWidth = 2
     }
 
     override func didReceiveMemoryWarning() {
